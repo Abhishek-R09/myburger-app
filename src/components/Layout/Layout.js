a7f16e02d14ee1dc930dc48171304bb735c0922a
Abhishek Ramasubramanian <0901abhishek.r@gmail.com>
2020-12-02 22:57:51 +0530

    Add some burger components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import Aux from "../../hoc/Auxiliary";
+import classes from "./Layout.module.css";
 const layout = (props) => {
   return (
     <Aux>
       <div>Toolbar, SideDrawer, Backdrop </div>
-      <main>{props.children}</main>
+      <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
 };
